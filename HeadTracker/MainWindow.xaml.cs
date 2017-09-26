23ce840dc69acf90ae36e32a8df71c2e0a25300f
TheAIBot <andreasuranus@hotmail.com>
2017-09-26 02:01:59 +0200

    s

--- HeadTracker/MainWindow.xaml.cs
+++ HeadTracker/MainWindow.xaml.cs
@@ -38,21 +38,21 @@ namespace HeadTracker
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            Bitmap test = new Bitmap("test2.png");
-            ct = new ColorClusterCreator(test.Width, test.Height);
+            //Bitmap test = new Bitmap("test2.png");
+            //ct = new ColorClusterCreator(test.Width, test.Height);
 
-            Stopwatch w = new Stopwatch();
-            w.Start();
+            //Stopwatch w = new Stopwatch();
+            //w.Start();
 
-            for (int i = 0; i < 200; i++)
-            {
-                ct.UpdateClusters(test);
-                ct.BitmapFromClusterMap();
-            }
+            //for (int i = 0; i < 200; i++)
+            //{
+            //    ct.UpdateClusters(test);
+            //    ct.BitmapFromClusterMap();
+            //}
 
-            w.Stop();
-            ct.BitmapFromClusterMap().Save("testResult.png");
-            MessageBox.Show(w.ElapsedMilliseconds.ToString());
+            //w.Stop();
+            //ct.BitmapFromClusterMap().Save("testResult.png");
+            //MessageBox.Show(w.ElapsedMilliseconds.ToString());
 
 
             FilterInfoCollection videoSources = new FilterInfoCollection(FilterCategory.VideoInputDevice);
